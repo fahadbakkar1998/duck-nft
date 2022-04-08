9731fd1a41ce82a6a72f7f5b85c8e45b873782eb
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-04-07 21:53:25 -0700

    main screen styling

--- src/components/CardImageSection/CardImageSection.tsx
+++ src/components/CardImageSection/CardImageSection.tsx
@@ -6,19 +6,12 @@ import "./CardImageSection.scss";
 import Footer from "./Footer";
 
 
-const CardImageSection = (props: any) => {
-  const mainScreen = useRef<HTMLDivElement>(null);
+const CardImageSection = (props: any) => {  
   const tozziDuckData = useMachineStore((state) => state.tozziDuckData);
-
-  const setOverflow = (flag: any) => {
-    flag
-      ? (mainScreen.current!.style!.overflow = "auto")
-      : (mainScreen.current!.style!.overflow = "hidden");
-  };
-
-  const [filterTozziDuckData, setFilterTozziDuckData] =
+  const [isHovered, setIsHovered] = useState(false);
+  const [filterTozziDuckData] =
   useState<any>(tozziDuckData);
-  const [filterCustomDuckData, setFilterCustomDuckData] = useState<any>([]);
+  const [filterCustomDuckData] = useState<any>([]);
 
   return (
     <Html
@@ -36,32 +29,39 @@ const CardImageSection = (props: any) => {
       <div className="main">
         <div
           className="mainScreen"
-          ref={mainScreen}
+          style={{ overflow: isHovered ? 'scroll' : 'hidden' }}
           id="mainScreen"
-          onMouseEnter={() => setOverflow(true)}
-          onMouseLeave={() => setOverflow(false)}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
         >
-          <div className="imgContainer scanlines">
-            {filterTozziDuckData.map((item: any) => {
-              let img = require(`../../assets/img/ducks/crypto_duck_${
-                parseInt(item.id) + 1
-              }.svg`);
-              // console.log("image object: ", img);
-              return <DuckCard key={item.id} img={img} data={item} />;
-            })}
-            {filterCustomDuckData.map((item: any) => {
-              return (
-                <DuckCard
-                  key={item.id}
-                  img={item.image}
-                  data={item}
-                  isCustom={true}
-                />
-              );
-            })}
+          <div 
+            style={{ borderRadius: '15%' }} 
+            className="pointer-events-none absolute w-full  z-30  inner-shadow border-radius h-full" />
+                    
+          <div className="relative w-full h-full">
+            
+            <div className="grid grid-cols-3  gap-1 relative">
+              
+              {filterTozziDuckData.map((item: any) => {
+                let img = require(`../../assets/img/ducks/crypto_duck_${
+                  parseInt(item.id) + 1
+                }.svg`);              
+                return <DuckCard key={item.id} img={img} data={item} />;
+              })}
+              {filterCustomDuckData.map((item: any) => {
+                return (
+                  <DuckCard
+                    key={item.id}
+                    img={item.image}
+                    data={item}
+                    isCustom={true}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Html>
   );
