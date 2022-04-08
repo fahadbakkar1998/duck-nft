7758660a74d012c4e02a54cbbc15aa3063d9f1a1
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-04-07 20:45:12 -0700

    extract footer component

--- src/components/CardImageSection/CardImageSection.tsx
+++ src/components/CardImageSection/CardImageSection.tsx
@@ -3,24 +3,12 @@ import { Html } from "@react-three/drei";
 import DuckCard from "../DuckCard/DuckCard";
 import useMachineStore from "../../store";
 import "./CardImageSection.scss";
+import Footer from "./Footer";
+
 
 const CardImageSection = (props: any) => {
   const mainScreen = useRef<HTMLDivElement>(null);
-  const address = useMachineStore((state) => state.address);
-
   const tozziDuckData = useMachineStore((state) => state.tozziDuckData);
-  const setCurrentTozziDuckId = useMachineStore(
-    (state) => state.setCurrentTozziDuckId
-  );
-
-  const customDuckData = useMachineStore((state) => state.customDuckData);
-  const setCurrentCustomDuckId = useMachineStore(
-    (state) => state.setCurrentCustomDuckId
-  );
-
-  const [filterTozziDuckData, setFilterTozziDuckData] =
-    useState<any>(tozziDuckData);
-  const [filterCustomDuckData, setFilterCustomDuckData] = useState<any>([]);
 
   const setOverflow = (flag: any) => {
     flag
@@ -28,9 +16,9 @@ const CardImageSection = (props: any) => {
       : (mainScreen.current!.style!.overflow = "hidden");
   };
 
-  useEffect(() => {
-    setFilterTozziDuckData(tozziDuckData);
-  }, [tozziDuckData]);
+  const [filterTozziDuckData, setFilterTozziDuckData] =
+  useState<any>(tozziDuckData);
+  const [filterCustomDuckData, setFilterCustomDuckData] = useState<any>([]);
 
   return (
     <Html
@@ -73,92 +61,7 @@ const CardImageSection = (props: any) => {
             })}
           </div>
         </div>
-        <div className="footer">
-          <div
-            onClick={() => {
-              setCurrentTozziDuckId(-1);
-              setCurrentCustomDuckId(-1);
-              setFilterTozziDuckData(tozziDuckData);
-              tozziDuckData.length &&
-                setCurrentTozziDuckId(tozziDuckData[0].id);
-              setFilterCustomDuckData([]);
-            }}
-          >
-            All
-          </div>
-          <div
-            onClick={() => {
-              setCurrentTozziDuckId(-1);
-              setCurrentCustomDuckId(-1);
-              const filterTozzi = tozziDuckData.filter((e) => !e.owner);
-              // const filterCustom = customDuckData.filter((e) => !e.owner);
-              setFilterTozziDuckData(filterTozzi);
-              // setFilterCustomDuckData(filterCustom);
-              setFilterCustomDuckData([]);
-              // filterTozzi.length
-              //   ? setCurrentTozziDuckId(filterTozzi[0].id)
-              //   : filterCustom.length &&
-              //     setCurrentCustomDuckId(filterCustom[0].id);
-              filterTozzi.length && setCurrentTozziDuckId(filterTozzi[0].id);
-            }}
-          >
-            Available
-          </div>
-          <div
-            onClick={() => {
-              setCurrentTozziDuckId(-1);
-              setCurrentCustomDuckId(-1);
-              const filterTozzi = tozziDuckData.filter((e) => !!e.owner);
-              // const filterCustom = customDuckData.filter((e) => !!e.owner);
-              setFilterTozziDuckData(filterTozzi);
-              // setFilterCustomDuckData(filterCustom);
-              setFilterCustomDuckData([]);
-              // filterTozzi.length
-              //   ? setCurrentTozziDuckId(filterTozzi[0].id)
-              //   : filterCustom.length &&
-              //     setCurrentCustomDuckId(filterCustom[0].id);
-              filterTozzi.length && setCurrentTozziDuckId(filterTozzi[0].id);
-            }}
-          >
-            Sold
-          </div>
-          <div
-            onClick={() => {
-              setCurrentTozziDuckId(-1);
-              setCurrentCustomDuckId(-1);
-              const filterTozzi = tozziDuckData.filter((e) => {
-                const owner = e.owner && e.owner.toLowerCase();
-                const me = address && address.toLocaleLowerCase();
-                return owner === me;
-              });
-              const filterCustom = customDuckData.filter((e) => {
-                const owner = e.owner && e.owner.toLowerCase();
-                const me = address && address.toLocaleLowerCase();
-                return owner === me;
-              });
-              setFilterTozziDuckData(filterTozzi);
-              setFilterCustomDuckData(filterCustom);
-              filterTozzi.length
-                ? setCurrentTozziDuckId(filterTozzi[0].id)
-                : filterCustom.length &&
-                  setCurrentCustomDuckId(filterCustom[0].id);
-            }}
-          >
-            My Ducks
-          </div>
-          <div
-            onClick={() => {
-              setCurrentTozziDuckId(-1);
-              setCurrentCustomDuckId(-1);
-              setFilterTozziDuckData([]);
-              setFilterCustomDuckData(customDuckData);
-              customDuckData.length &&
-                setCurrentCustomDuckId(customDuckData[0].id);
-            }}
-          >
-            Custom
-          </div>
-        </div>
+        <Footer />
       </div>
     </Html>
   );
