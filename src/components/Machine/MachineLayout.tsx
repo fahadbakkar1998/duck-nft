88dc2e5217dcff8b82959c834afdf8644b53a5b8
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-04-12 22:28:05 -0700

    add oscilliscope thing

--- src/components/Machine/MachineLayout.tsx
+++ src/components/Machine/MachineLayout.tsx
@@ -4,6 +4,8 @@ import BurnModal from "../BurnModal";
 import useMachineStore from "../../store";
 import ColorPicker from "../DrawingTool/ColorPicker";
 import { MachineMode } from "../../utils/constants";
+import WaveForm from "./WaveForm";
+
 
 export const MachineLayout = () => {
 
@@ -13,7 +15,8 @@ export const MachineLayout = () => {
     <group>      
       <AltScreen />
       <MainScreen />     
-      { currentMode === MachineMode.Customization && <ColorPicker /> }       
+      { currentMode === MachineMode.Customization && <ColorPicker /> }
+      { currentMode !== MachineMode.Customization  && <WaveForm />} 
       {/* <BurnModal
         openModal={openBurnModal}
         onCloseModal={() => { setOpenBurnModal(false)}}
---
+++ src/components/Machine/WaveForm.tsx
@@ -0,0 +1,30 @@
+import { Html } from "@react-three/drei";
+import { useThree } from "react-three-fiber";
+import {  minViewLength } from "../../utils/constants";
+
+const WaveForm: () => JSX.Element = () => {  
+  const { viewport } = useThree();
+  const min = viewport.width;
+
+  return (
+    <Html
+      scale={[
+        (0.18 * min) / minViewLength,
+        (0.18 * min) / minViewLength,
+        (0.18 * min) / minViewLength,
+      ]}
+      position={[-0.144 * min, -0.22 * min, 0]}
+      rotation={[0.0, 0.0, 0.0]}
+      transform
+    >
+      <div
+        style={{ borderRadius: 13 }}       
+        className="h-[115px] w-[110px] bg-black border-gray-600 border-2"                
+      >
+        <video muted autoPlay playsInline className="aspect-square w-full h-full" src="/assets/video/wave.mp4" />
+      </div>
+    </Html>
+  );
+};
+
+export default WaveForm;
