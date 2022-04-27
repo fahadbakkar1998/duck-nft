ba1a1f748cad2b2847ee3240d6ac8e6b2bd21342
JSilver-Legend <procaptain516@gmail.com>
2022-04-26 18:52:29 -0700

    add display [anel

--- src/components/Machine/Display/index.tsx
+++ src/components/Machine/Display/index.tsx
@@ -1,13 +1,26 @@
 import { Html } from "@react-three/drei";
 import styles from './styles.module.scss';
+import { minViewLength } from "../../../utils/constants";
+import { useThree } from "react-three-fiber";
 
 type Props = {
     value?: number;
 }
 
 const Display: React.FC<Props> = ({ value }) => {
+
+  const { viewport } = useThree();  
+  const min = viewport.width;
+  
   return (
-    <Html>
+    <Html
+      scale={[
+        1,
+        1,
+        1
+      ]}
+      transform
+    >
         <div id='display' className={styles.mainWrap}>
             {value === 0 ? '' : value}
         </div>
