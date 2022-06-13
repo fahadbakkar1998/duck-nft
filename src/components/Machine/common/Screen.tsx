ffb7e18c4ac7a62c0b7eadd1f041570ff150103f
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-06-13 16:29:13 -0700

    remove screenwrapper

--- src/components/Machine/common/Screen.tsx
+++ src/components/Machine/common/Screen.tsx
@@ -2,15 +2,12 @@ import { FC } from 'react';
 import { Html } from '@react-three/drei';
 import { HtmlProps } from '@react-three/drei/web/Html';
 import Providers from '../../../Providers';
-import ScreenWrapper from './ScreenWrapper';
 
 const Screen: FC<HtmlProps> = (props) => {
   return (
     <Html {...props}>
       <Providers>
-        <ScreenWrapper>
-          {props.children}
-        </ScreenWrapper>
+        {props.children}
       </Providers>
     </Html>
   );
