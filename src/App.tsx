af0940e17f938c10de9b956617de30ac03982ec2
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-08-04 22:11:29 -0700

    lock out safari

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.scss';
-import { isMobile } from 'react-device-detect';
+import { isMobile, isSafari } from 'react-device-detect';
 import Desktop from './components/layout/Desktop';
 import Mobile from './components/Mobile';
 
 const App = () => {
-  return isMobile ? <Mobile /> : <Desktop />;
+  return isMobile || isSafari ? <Mobile /> : <Desktop />;
 };
 
 export default App;
