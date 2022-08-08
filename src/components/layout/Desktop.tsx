3a353ea9b37316bba9d06c9aca8d2b11282453c8
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-08-08 13:54:13 -0700

    overlay

--- src/components/layout/Desktop.tsx
+++ src/components/layout/Desktop.tsx
@@ -2,12 +2,15 @@ import { Suspense } from 'react';
 import { Canvas } from 'react-three-fiber';
 import { Cloud } from '@react-three/drei';
 import Machine from '../Machine/index';
+// eslint-disable-next-line import/no-relative-packages
+import { motion, AnimatePresence } from '../../../node_modules/framer-motion/dist/framer-motion';
 import bgImg from '../../assets/img/machine-bg.jpg';
 import OwnersManualModal from '../OwnersManual/OwnersManualModal';
 import useMachineStore from '../../store';
 import BTI from '../Machine/BinaryToggleInterface';
 import TitleImage from './TitleImage';
 import Footer from './Footer';
+import overlay1 from '../../assets/img/overlay_1.png';
 
 const Desktop = () => {
   const {
@@ -16,28 +19,40 @@ const Desktop = () => {
   } = useMachineStore();
 
   return (
-    <div className="app-container overflow-hidden">
-      <div
-        className="mx-auto machine-container w-full 2xl:w-[80%]"
-      >
-        <BTI />
-        <TitleImage />
-        <img className="z-0 absolute top-0 left-0 pointer-events-none" src={bgImg} alt="" />
-        <Canvas
-          className="absolute -top-[18.85%]"
-          orthographic
-          camera={{ zoom: 100, position: [0, 0, 100] }}
-          onCreated={(state) => state.gl.clearColor()}
-        >
-          <Suspense fallback={null}>
-            <pointLight intensity={4} position={[-10, 10, 5]} />
-            <ambientLight intensity={0.6} />
-            <Machine />
-          </Suspense>
-        </Canvas>
-        <Footer />
+    <div className="bg-opacity">
+      {/* <div className="w-full h-full absolute bg-black bg-opacity-70 pointer-events-none" style={{ zIndex: 100 }} /> */}
+      <div className="app-container overflow-hidden">
+        <div className="mx-auto machine-container w-full 2xl:w-[80%]">
+          <AnimatePresence>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              style={{ zIndex: 100 }}
+              className="absolute w-full drop-shadow pointer-events-none "
+            >
+              <img src={overlay1} alt="overvlay" />
+            </motion.div>
+          </AnimatePresence>
+          <BTI />
+          <TitleImage />
+          <img className="z-0 absolute top-0 left-0 pointer-events-none" src={bgImg} alt="" />
+          <Canvas
+            className="absolute z-10 -top-[18.85%]"
+            orthographic
+            camera={{ zoom: 100, position: [0, 0, 100] }}
+            onCreated={(state) => state.gl.clearColor()}
+          >
+            <Suspense fallback={null}>
+              <pointLight intensity={4} position={[-10, 10, 5]} />
+              <ambientLight intensity={0.6} />
+              <Machine />
+            </Suspense>
+          </Canvas>
+          <Footer />
+        </div>
+        <OwnersManualModal modalIsOpen={isOwnersManualOpen} setModalIsOpen={setIsOwnersManualOpen} />
       </div>
-      <OwnersManualModal modalIsOpen={isOwnersManualOpen} setModalIsOpen={setIsOwnersManualOpen} />
     </div>
   );
 };
