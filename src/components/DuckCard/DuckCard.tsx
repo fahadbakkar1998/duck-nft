364b0034ae2d203c3e05512fb6a3afc712712ac5
fahadbakkar1998 And <Fahadbakkar48@gmail.com>
2022-08-31 21:38:13 -0700

    200 sold

--- src/components/DuckCard/DuckCard.tsx
+++ src/components/DuckCard/DuckCard.tsx
@@ -13,7 +13,7 @@ const DuckCard: FC<DuckData> = (data) => {
     setCurrentDuckId(data.id);
   };
 
-  const unsoldCollabs = [200, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219];
+  const unsoldCollabs = [202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219];
 
   const showSoldOverlay = data.owner && showAvailabilityOnDuckCards && !unsoldCollabs.includes(data.id);
 
